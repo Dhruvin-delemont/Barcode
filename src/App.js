4dfd1c0a50216e71c1c44e6e8ae6e52961a49bdf
Dhruvin-delemont <dhruvin@delemontstudio.com>
2023-08-19 10:36:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import BarcodeScannerComponent from 'react-barcode-reader';
+import React, { useEffect } from 'react';
+import BarcodeScannerComponent from 'react-qr-barcode-scanner';
 import './App.css';
 
 function App() {
-  const [data, setData] = React.useState('Not Found');
+  const [data, setData] = React.useState('Camera is not detected.');
 
+  const handleUpdate = (err, result) => {
+    if (result) {
+      setData(result.text);
+    } else {
+      setData('Camera is not detected.');
+    }
+  };
   return (
     <>
       <div className="camera-main">
         <BarcodeScannerComponent
+          className="web-cam"
           width={500}
           height={500}
-          onUpdate={(err, result) => {
-            result ? setData(result.text) : setData('Not Found');
-          }}
+          onUpdate={handleUpdate}
         />
       </div>
-      <p>{data}</p>
+      {data === 'Camera is not detected.' && <p className={`message ${data === 'Camera is not detected.' ? 'red' : ''}`}>{data}</p>}
     </>
   );
 }
