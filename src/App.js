eb74a1a14dd3f725a5c5b57474e398d0a1fa822d
Dhruvin-delemont <dhruvin@delemontstudio.com>
2023-08-21 10:15:55 +0530

    add pop model

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import BarcodeScannerComponent from 'react-qr-barcode-scanner';
 import './App.css';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function App() {
-  const [data, setData] = React.useState('Not Found');
+  const [data, setData] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  let link = 'https://www.jaypeebrothers.com/pgDetails.aspx?cat=s&book_id=';
+
+  React.useEffect(() => {
+    // check type of data
+    if (typeof data == 'number') {
+      setTimeout(() => {
+        handleOpen();
+      });
+    }
+  }, [data]);
+
+  const handleRedirect = () => {
+    window.open(link + data, '_blank');
+  };
 
   return (
     <>
@@ -12,12 +46,35 @@ function App() {
           width={500}
           height={500}
           onUpdate={(err, result) => {
-            if (result) setData(result.text);
-            else setData('Not Found');
+            if (result) {
+              setData(Number(result.text));
+            } else {
+              setData(null);
+            }
           }}
         />
       </div>
-      <p className="message red">{data}</p>
+      <p
+        style={{ color: !data ? 'red' : 'green' }}
+        className="message">
+        {!data ? 'Please Scan Barcode!' : `Barcode No.: ${data}`}
+      </p>
+
+      {/* Pop ups model */}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box sx={style}>
+          <Typography id="modal-modal-title">Press Allow to Open this site!</Typography>
+          <br />
+          <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+            <Button onClick={handleClose}>Close</Button>
+            <Button onClick={handleRedirect}>Allow</Button>
+          </Box>
+        </Box>
+      </Modal>
     </>
   );
 }
